a0bb2ff8bd0dcbb8125c000a997a26ca4697bfa1
pragatik <pskhole99@gmail.com>
2025-07-21 17:18:51 +0530

    add the section blog and form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
+import lineClamp from "@tailwindcss/line-clamp";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lora: ["Lora", "serif"],
+        mulish: ["Mulish", "sans-serif"],
+        opensans: ['"Open Sans"', "sans-serif"],
+      },
+      colors: {
+        olive: "#7E9E86",
+        "olive-dark": "#5F7C66",
+        light: "#FEFEFE",
+        "custom-pink": "#FFC7FD",
+        "light-green": "#E3F2E1",
+      },
+    },
   },
-  plugins: [],
+  plugins: [lineClamp],
 };
