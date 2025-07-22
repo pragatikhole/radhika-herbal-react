7c407ecd6c5ef4665611ef1991c66cedc6e46c6b
Swati kadam <swatimarotikadam@gmail.com>
2025-07-22 17:43:35 +0530

    our products added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import OurProducts from "./components/ourproducts"; // Capitalized import
+import ProductsGrid from "./components/ProductsGrid";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world! its radhika herbal project</h1>;
+  return (
+    <div className="p-6">
+      
+      <div className="bg-gray-50 min-h-screen p-4">
+  <div className="max-w-6xl mx-auto">
+    <OurProducts />
+    <ProductsGrid />
+  </div>
+</div>
+
+      
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+// tailwind.config.js
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"', "serif"],
+      },
+      colors: {
+        herbal: "#f7941e",
+      },
+    },
   },
   plugins: [],
 };
